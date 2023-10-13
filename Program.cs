96d3ad410555c2e4d0e923d6891d52bdb45ddea2
Akhlaq Khan <akhlaqkhan@refactory.id>
2023-10-13 20:35:38 +0700

    use latest

--- Program.cs
+++ Program.cs
@@ -1,26 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello Akhlaq Khan!");
 
 app.Run();
-//test
-//test
-//test
-//test
-//test
-//test
-//test
-//test
-//test
-//test
-//test
-//test
-//test
-//test
-//test
-//test
-//test
-//test
-//test
-//test
