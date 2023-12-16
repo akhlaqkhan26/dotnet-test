686f23bc30c796c8cf1bb5eca4591b5a786f6aad
akhlaqkhan26 <106056647+akhlaqkhan26@users.noreply.github.com>
2023-12-16 08:04:52 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello Akhlaq Khan!");
+app.MapGet("/", () => "Hello World");
 
 app.Run();
 //test
